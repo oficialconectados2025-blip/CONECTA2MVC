8d52462a1394eff749227021939e6951f47014f8
Jonathan281003Lazo <jonathanjorgemora@gmail.com>
2025-10-17 05:04:32 -0600

    Nuevos ajustes dentro de la arquitectura, estamos culminando la base solida y dandole vida a la creacion de usuarios y asignacion de roles, aun no hemos pasado a la parte de que los roles cumplan con el proposito de inhabilitar o habilitar ciertas funciones para cada usuario

---
+++ CONECTA2MVC.Datos/Implementaciones/GenericRepository.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CONECTA2MVC.Datos.DBContextCONECTA2;
+using CONECTA2MVC.Datos.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace CONECTA2MVC.Datos.Implementaciones
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+
+        private readonly DBContextConecta2 _context;
+
+        public GenericRepository(DBContextConecta2 context)
+        {
+
+            _context = context;
+            
+        }
+
+        public async Task<IQueryable<TEntity>> Consultar(Expression<Func<TEntity, bool>> filt)
+        {
+
+            IQueryable<TEntity> queryEntidad = filt == null ? _context.Set<TEntity>() : _context.Set<TEntity>().Where(filt);
+            return queryEntidad;
+
+        }
+
+        public async Task<TEntity> Crear(TEntity entidad)
+        {
+
+            try
+            {
+
+                _context.Set<TEntity>().Add(entidad);
+                await _context.SaveChangesAsync();
+                return entidad;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public async Task<bool> Editar(TEntity entidad)
+        {
+            try
+            {
+
+                _context.Update(entidad);
+                await _context.SaveChangesAsync();
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<bool> Eliminar(TEntity entidad)
+        {
+            try
+            {
+
+                _context.Remove(entidad);
+                await _context.SaveChangesAsync();
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<TEntity> Obtener(Expression<Func<TEntity, bool>> filt)
+        {
+            try
+            {
+                TEntity entidad = await _context.Set<TEntity>().FirstOrDefaultAsync(filt);
+                return entidad;
+            }  
+            catch (Exception ex) 
+            {
+                throw ex;
+            }
+        }
+    }
+}
+
---
+++ CONECTA2MVC.Datos/Interfaces/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Linq.Expressions;
+
+namespace CONECTA2MVC.Datos.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> Obtener(Expression<Func<TEntity, bool>> filt);
+        Task<TEntity> Crear(TEntity entidad);
+        Task<bool> Eliminar(TEntity entidad);
+        Task<bool> Editar(TEntity entidad);
+        Task<IQueryable<TEntity>> Consultar(Expression<Func<TEntity, bool>> filt);
+    }
+}
